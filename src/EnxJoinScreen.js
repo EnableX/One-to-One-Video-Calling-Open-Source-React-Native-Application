b608e55e8740fc66d0110602e0472f53c3dea9d6
Pankaj Pandey <pankaj.p@vcloudx.com>
2022-02-03 17:13:12 +0530

    update enxrtc

--- src/EnxJoinScreen.js
+++ src/EnxJoinScreen.js
@@ -315,6 +315,6 @@ const styles = StyleSheet.create({
   const kTry = true;
   /*Use enablec portal to create your app and get these following credentials*/
 
-  const  kAppId = "App_ID";
-  const  kAppkey = "App_key";
+  const  kAppId = "5ef5b31690ef80b4300b0bd2";
+  const  kAppkey = "uJehyWaAu4uvyTupeJyJuHu6ygyYaGu2yzuq";
 
